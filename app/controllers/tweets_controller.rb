e1814731d270bf7c4c2403333021bc05f2a2973c
sc30gsw <haidoushi875@gmail.com>
2021-03-07 06:17:06 +0900

    アクションの設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,13 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
 end
