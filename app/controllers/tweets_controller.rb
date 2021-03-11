58a7b0f119546d23dee72fc57a75c38b5d48db5f
sc30gsw <haidoushi875@gmail.com>
2021-03-11 13:35:13 +0900

    未ログインユーザーの処理の設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:destroy, :edit, :update, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @tweets = Tweet.all
   end
