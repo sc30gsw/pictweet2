59854247421cb49ccfc9ae668af7f143e4e13d6e
sc30gsw <haidoushi875@gmail.com>
2021-03-11 13:31:04 +0900

    ストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_parameter_permitted, if: :devise_controller?
+
+  private
+
+  def configure_parameter_permitted
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
