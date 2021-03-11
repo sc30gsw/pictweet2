6085a17f1c0d3ad0528dd1a070cbef761f8e562e
sc30gsw <haidoushi875@gmail.com>
2021-03-12 06:56:43 +0900

    メソッドの定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,4 +5,12 @@ class Tweet < ApplicationRecord
   end
   has_many :comments
   belongs_to :user
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
