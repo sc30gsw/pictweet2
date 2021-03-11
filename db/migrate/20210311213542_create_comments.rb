7f4cf979ab6666ecb27612bfff0decd3789cf63f
sc30gsw <haidoushi875@gmail.com>
2021-03-12 06:38:28 +0900

    Commentsテーブルの作成

--- db/migrate/20210311213542_create_comments.rb
+++ db/migrate/20210311213542_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.references :user, foreign_key: true
+      t.references :tweet, foreign_key: true
+      t.text :text
       t.timestamps
     end
   end
